39bfe026733e69c58777c4c5fb89ebf209abcb13
Terentev A. A. <terentev.a.a@recloud.tech>
2025-10-03 08:25:05 +0300

    Remove `.idea/workspace.xml` from the repository

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,7 +30,7 @@ const config = {
   organizationName: 'RecloudTech', // Usually your GitHub org/user name.
   projectName: 'recloud-wiki', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
